4c8c96caf81db1cfa635853dc0cfdd23b40159e8
MuhammadYusuf-Hakimov <muhammadyusuf201277711@gmail.com>
2026-07-25 17:24:21 +0500

    Muhammadyusuf

--- 2-bo'lim/lesson4/index.js
+++ 2-bo'lim/lesson4/index.js
@@ -1,1 +1,50 @@
-console.log("Salom");
+// startsWith - matn ning boshi biz ber gan qiymat bilan boshlanadimi? shuni tekshiradi
+// let a = "+998 99 111 22 33";
+// let b = "+1239 23293 88";
+// let c = "+7 239 23293 88";
+
+// console.log(a.startsWith("+998"));
+// console.log(b.startsWith("+998"));
+// console.log(c.startsWith("+998"));
+
+// endsWith - matn ning oxiri biz ber gan qiymat bilan tugaydimi? shuni tekshiradi
+
+// let email1 = "eshmat@gmail.com"
+// let email2 = "toshmat@yandex.ru"
+// let email3 = "gulmat@mail.com"
+
+// console.log(email1.endsWith("@gmail.com"));
+// console.log(email2.endsWith("@gmail.com"));
+// console.log(email3.endsWith("@gmail.com"));
+
+// search - matn ichidan ixtiyoriy qiymat, topsa index ni qaytaradi.
+// let str = "*Oxf*ord*";
+// console.log(str.search("f"));
+// console.log(str.search("w"));
+
+// indexOf, lastindexOf - matn ichidan qiymat qidiradi, topsa index qaytaradi
+// topolmasa -1 qaytaradi, oldi va orqa tomondan qidirish usullari va aynan 
+// qaysi index boshlab qidirishini ham aytsa bo'ladi.
+// console.log(str.indexOf("f"));
+// console.log(str.indexOf("w"));
+// console.log(str.indexOf("*", 5))   
+// console.log(str.lastIndexOf("*", 7))
+
+// padStart, padEnd - matn boshiga va oxiriga qiymat qo'shish uchun kerak.
+// let n = "91 111 22 33"
+// n = n.padStart(6 + n.length, "+998");
+// console.log(n);
+
+// let email = "eshmatbek";
+// email = email.padEnd(email.length + 10, "@gmail.com");
+// console.log(email);
+
+// repeat - qiymatni takrorlash uchun kerak.
+// let n = 10;
+// console.log(n.toString().repeat(10));
+
+// replace - bir qiymatni ikkinchisiga almashtirish.
+// let str = "Men.bugun.Oxford.markazida.IT.darsida.o'tiribman!";
+// str = str.replace(".", " ");
+// str = str.replaceAll(".", " ");
+// console.log(str);
---
+++ 2-bo'lim/lesson4/vazifa.js
@@ -0,0 +1,26 @@
+let country = "Uzbekistan";
+console.log(country.startsWith("Uz"));
+
+let file = "photo.png";
+console.log(file.endsWith(".png"));
+
+let text = "JavaScript";
+console.log(text.search("S"));
+
+let word = "Programming";
+console.log(word.indexOf("g"));
+
+let fruit = "banana";
+console.log(fruit.lastIndexOf("a"));
+
+let number = "45";
+console.log(number.padStart(5, "0"));
+
+let name = "Ali";
+console.log(name.padEnd(6, "!"));
+
+let star = "*";
+console.log(star.repeat(10));
+
+let sentence = "Salom dunyo";
+console.log(sentence.replace("dunyo", "JavaScript"));
