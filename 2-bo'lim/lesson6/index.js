1fe0c5272ef5cd1c22917b4bc2e6026a580d7c71
MuhammadYusuf-Hakimov <muhammadyusuf201277711@gmail.com>
2026-07-28 18:38:15 +0500

    masala3

--- 2-bo'lim/lesson6/index.js
+++ 2-bo'lim/lesson6/index.js
@@ -8,12 +8,23 @@
 // }
 //  reverseStr(str)
 
-let str = "Oxford";
-let i = 0;
-function geti(str){
-    for(let value of str){
-    i+=value.charCodeAt()
-}
-return i;
-}
-console.log(geti(str));
+// let str = "Oxford";
+// let i = 0;
+// function geti(str){
+//     for(let value of str){
+//     i+=value.charCodeAt()
+// }
+// return i;
+// }
+// console.log(geti(str));
+
+// let str = "Algoritm and Oxford";
+// let u = "";
+// function toq(str) {
+//     for (let i = 0; i < str.length; i += 2) {
+//         u += str[i];
+//     }
+//     return u
+// }
+// toq(str)
+// console.log(u);
