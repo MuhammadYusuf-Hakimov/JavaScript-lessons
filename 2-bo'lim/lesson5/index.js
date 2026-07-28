4bf6b2ea44f3a1a252b83f143cfb2e36a2926a44
MuhammadYusuf-Hakimov <muhammadyusuf201277711@gmail.com>
2026-07-28 13:34:09 +0500

    Muhammadyusuf

---
+++ 2-bo'lim/lesson5/index.js
@@ -0,0 +1,56 @@
+// slice - matn dan nusxa olish uchun ishlatiladi.
+// let str = "Oxford Lerning Center";
+// let copytext = str.slice(0, 6);
+// // slice(startIndex, endIndex + 1);
+// console.log(copytext);
+
+// console.log(str.slice(7, 15));
+
+// console.log(str.slice(16, 22));
+// console.log(str.slice(-6));
+// console.log(str.slice(16));
+
+// console.log(str.slice(7));
+
+// let i = str.indexOf("L");
+// console.log(str.slice(i));
+
+// substring- slice bilan bir xil ishlaydi faqat manfiy qiymat qabul qilmaydi.
+// console.log(str.substring(-6));  x
+
+// split - Stringni Array ga aylantirib beradi.
+// let str = "Oxford";
+// ['O', 'x', 'f', 'o', 'r', 'd']
+// let arr = str.split("");
+// console.log(arr);
+
+// let str = "Men Oxford Js o'rganmoqdaman"
+// let arr = str.split(" ")
+// console.log(arr);
+
+// trim - str boshi va oxiridan bo'sh joylarni tozalab yuboradi.
+// let str = "     Oxford va Algoritm            "
+// console.log(str);
+// console.log(str.length);
+// console.log(str.trim());
+// console.log(str.trim().length);
+
+// let num = 234567
+// console.log(num.toString());
+
+// let str = "Salom";
+// console.log(str.toUpperCase());
+// console.log(str.toLocaleLowerCase());
+
+// loclaCompare - str ma'lumotnni ASCCII code bo'yicha taqqoslaydi.
+// -1, 0 ,1
+
+// let a = "B"
+// let b= "A"
+// console.log(a.localeCompare(b));
+
+// console.log("1".localeCompare("1"));
+
+// eval
+// let sum = "19 - 9 + (2 * 5) / 10"
+// console.log(eval(sum));
