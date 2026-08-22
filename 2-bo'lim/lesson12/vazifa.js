62733513d2531c96f44f080d4df6b5a38f565076
MuhammadYusuf-Hakimov <muhammadyusuf201277711@gmail.com>
2026-08-22 13:40:45 +0500

    Vazifa

---
+++ 2-bo'lim/lesson12/vazifa.js
@@ -0,0 +1,35 @@
+// let arr = ["Olma", "Uzum", "Nok", "Anor", "Nok"];
+// let findResult = arr.find(value => value === "Nok");
+// console.log(findResult);
+
+// let findIndexResult = arr.findIndex(value => value === "Nok");
+// console.log(findIndexResult);
+
+// let findLastIndexResult = arr.findLastIndex(value => value === "Nok");
+// console.log(findLastIndexResult);
+
+// let numbers = [20, 7, 50, 5, 1];
+// numbers.sort((a, b) => a - b);
+// console.log(numbers);
+
+// let everyResult = [2, 4, 6, 8].every(value => value % 2 === 0);
+// console.log(everyResult);
+
+// let someResult = [1, 3, 5, 8].some(value => value % 2 === 0);
+// console.log(someResult);
+
+// let nestedArr = [1, [2, [3, [4, [5]]]]];
+// let flatResult = nestedArr.flat(Infinity);
+// console.log(flatResult);
+
+// let newArr = Array(5);
+// console.log(newArr);
+
+// let fillArr = Array(5);
+// fillArr.fill("Olma");
+// console.log(fillArr);
+
+// let copyArr = [1, 2, 3, 4, 5, 6];
+
+// copyArr.copyWithin(0, 4);
+// console.log(copyArr);
